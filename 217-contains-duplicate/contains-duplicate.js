50f3c43c1fd087e38668d3602efb69d52699733d
TRIDEEP GHOSH <41672219+3deep0019@users.noreply.github.com>
2026-06-08 02:07:11 +0530

    Time: 39 ms (15.06%) | Memory: 77.7 MB (13.79%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+        const seen = {};
+    for (const num of nums) {
+        if (seen[num]) {
+            return true;
+        } else {
+            seen[num] = 1;
+        }
+    }
+    return false;
+};

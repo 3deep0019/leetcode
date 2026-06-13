48af1086640aa3569197cf097749a758aa151bd7
TRIDEEP GHOSH <41672219+3deep0019@users.noreply.github.com>
2026-06-13 22:00:30 +0530

    Time: 3 ms (69.52%) | Memory: 55.6 MB (56.28%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    const pairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let char of s) {
+        if (pairs[char]) {
+            stack.push(char);
+        } else {
+            const lastOpen = stack.pop();
+            if (pairs[lastOpen] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};

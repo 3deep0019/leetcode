d06c0054bfe8e9a62e7bf2012eb57ff57efe268a
TRIDEEP GHOSH <41672219+3deep0019@users.noreply.github.com>
2026-06-15 23:43:44 +0530

    Time: 1 ms (59.98%) | Memory: 62.5 MB (13.40%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,14 +3,10 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let i = 0;
-    let j = s.length - 1;
-    while(i < j){
-        let temp = s[j];
-        s[j] = s[i];
-        
-        s[i] = temp;
-        i++;
-        j--;
+    let left = 0, right = s.length - 1;
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
     }
 };

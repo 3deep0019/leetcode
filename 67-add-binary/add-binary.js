a28a4426e38aaf3776ac9c05074a3c2b429c3bfe
TRIDEEP GHOSH <41672219+3deep0019@users.noreply.github.com>
2026-06-30 16:59:35 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (88.87%) - LeetSync

--- 67-add-binary/add-binary.js
+++ 67-add-binary/add-binary.js
@@ -9,3 +9,25 @@ var addBinary = function(a, b) {
     const sum = BigInt(aBin) + BigInt(bBin)
     return sum.toString(2)
 };
+
+// var addBinary = function (a, b) {
+//     let i = a.length - 1, j = b.length - 1;
+//     let carry = 0;
+//     const result = [];
+//     while (i >= 0 || j >= 0 || carry) {
+//         let total = carry;
+//         if (i >= 0) {
+//             console.log('a', a[i]);
+//             total += parseInt(a[i])
+//             i--
+//         }
+//         if (j >= 0) {
+//             console.log('b', b[j]);
+//             total += parseInt(b[j])
+//             j--
+//         }
+//         result.unshift(total % 2);
+//         carry = Math.floor(total / 2);
+//     }
+//     return result
+// };

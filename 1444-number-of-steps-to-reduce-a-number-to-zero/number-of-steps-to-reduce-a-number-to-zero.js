ce09859255460760bbc510c971026b9fd46c99aa
TRIDEEP GHOSH <41672219+3deep0019@users.noreply.github.com>
2026-07-06 13:54:13 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (67.01%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
@@ -3,18 +3,11 @@
  * @return {number}
  */
 var numberOfSteps = function(num) {
-    let count = 0;
-    let temp = num
-    for(let i=0;i<num+1;i++){
-        if(temp == 0){
-            return count;
-            break
-        }
-        if(temp % 2 == 0){
-            temp = Math.floor(temp/2);
-        }else{
-            temp-=1;
-        }
-        count++;
+    let step = 0;
+
+    while (num !== 0) {
+        num % 2 === 0 ? num /= 2 : num--;
+        step++;
     }
+    return step;
 };

8e88b80021bf242221aa09352af46058ad98b31e
TRIDEEP GHOSH <41672219+3deep0019@users.noreply.github.com>
2026-08-11 12:34:24 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (97.83%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.js
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.js
@@ -3,18 +3,23 @@
  * @return {number}
  */
 var missingInteger = function(nums) {
-    let longestPrefixSum = nums[0];
-    for (let i = 1; i < nums.length; i++){
-        if (nums[i] == nums[i - 1] + 1) {
-            longestPrefixSum += nums[i];
+    let sum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1] + 1) {
+            sum += nums[i];
         } else {
             break;
         }
     }
 
-    const set = new Set(nums)
-    while (set.has(longestPrefixSum)) {
-        longestPrefixSum++;
+    const seen = new Set(nums);
+
+    let answer = sum;
+
+    while (seen.has(answer)) {
+        answer++;
     }
-    return longestPrefixSum
+
+    return answer;
 };
